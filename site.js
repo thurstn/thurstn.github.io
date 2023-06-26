7f5abd93e7bd33dcf629295c0b868abc2bc9f487
Vinnie Fisher <71513636+thurstn@users.noreply.github.com>
2023-06-26 22:26:41 +0100

    big boy

--- site.js
+++ site.js
@@ -1,9 +1,43 @@
-var accordionSection = $('.acc-section')
+var sticky = document.getElementById("navbar").offsetTop;
 
-function ToggleAccordion(current)
-{
-    accordionSection.removeClass('active');
-    $(this).addClass('active');
+window.onscroll = function(){
+    let els = document.getElementsByClassName("showonscroll");
+    let logo = document.getElementById("logo");
+    if (isInViewport(logo))
+    {
+        for (let i = 0; i < els.length; i++)
+        {
+            els[i].style.visibility = "hidden";
+            els[i].style.opacity = 0;
+        }
+    }
+    else
+    {
+        for (let i = 0; i < els.length; i++)
+        {
+            els[i].style.visibility = "visible";
+            els[i].style.opacity = 1;
+        }
+    }
+
+    
+    if (window.scrollY >= sticky)
+    {
+        document.getElementById("navbar").classList.add("sticky");
+    }
+    else
+    {
+        document.getElementById("navbar").classList.remove("sticky");
+    }
 }
 
-accordionSection.on('click', ToggleAccordion);
+function isInViewport(element)
+{
+    const rect = element.getBoundingClientRect();
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+}
